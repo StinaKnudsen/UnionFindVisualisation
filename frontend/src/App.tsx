d69fac727061e4273848dee1bb601887cfd5e8b9
AVNBuelow <alby@itu.dk>
2026-03-29 22:33:50 +0200

    removed unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useEffect, useMemo, useState, useRef } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Node } from "./components/Node";
 import { Edge } from "./components/Edge";
 import type { GraphNode } from "./components/Node";
@@ -231,4 +231,3 @@ function App() {
 }
 
 export default App
-
