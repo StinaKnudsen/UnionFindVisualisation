d805d57854060240fd211489a62b742ed537994e
StinaKnudsen <stinaknudsen@gmail.com>
2026-03-30 10:53:47 +0200

    link between lanidng page and expore pages done

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Landing from "./pages/landingPage";
+import UFBuilderPage from "./pages/UFBuilderPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/builder/:ufType" element={<UFBuilderPage />} />
       </Routes>
     </Router>
   </StrictMode>
